ac53bb2ed0d573e55fea03ddffbe54c1d2e78a3e
Egor <egoart160810@gymhueck.it>
2025-10-07 13:07:54 +0200

    dd

---
+++ 04-kapitel/01-einfuehrung/code-1-der-datentyp-boolean.py
@@ -0,0 +1,22 @@
+"""
+Mit dem Datentyp bool experimentieren
+"""
+
+print(17 > 5)
+# Ausgabe der Konsole und deine Beobachtung.
+
+print(17 < 5)
+# Ausgabe der Konsole und deine Beobachtung.
+
+print(type(17 > 5))
+# Ausgabe der Konsole und deine Beobachtung.
+
+x = 17 > 5
+y = 17 < 5
+
+print(x, y)
+# Ausgabe der Konsole und deine Beobachtung.
+
+print(type(x), type(y))
+# Ausgabe der Konsole und deine Beobachtung.
+
